dc1cf5d577b5aac42a1f79a5e4aaaad04472aaf3
chenyuzhi459 <553673833@qq.com>
2017-09-18 20:02:42 +0800

    备份

--- src/components/data/access/TaskManager.vue
+++ src/components/data/access/TaskManager.vue
@@ -1,13 +1,88 @@
 <<template>
+<div>
+    <running-tasks></running-tasks>
 
-<running-tasks></running-tasks>
-  
+    <div style=" margin-left:20px;">
+        <el-button type="primary" size="small" @click="showCreateTaskDialog">Create Task</el-button>
+    </div>
+
+    <el-dialog  :visible.sync="createTaskDialogVisible" size="small" title="Task Json" :close-on-click-modal="false">
+        <el-button type="primary" @click="format">format</el-button> 
+        <span style="color: red" v-if="jsonSyntaxError">json格式错误</span>
+        <br></br>             
+        <el-input type="textarea" :rows="25" v-model="taskJson">
+        </el-input>
+        <span slot="footer" class="dialog-footer">
+                <el-button @click="createTaskDialogVisible = false">取 消</el-button> 
+            <el-button type="primary" @click="creatTask()">创 建</el-button>
+        </span>
+    </el-dialog>
+</div>
 </template>
 <<script>
+
 import RunningTasks from './taskmanager/RunningTasks'
+
 export default {
+  data () {
+    return {
+      jsonSyntaxError: false,
+      createTaskDialogVisible:false,
+      taskJson:''
+    }
+  },
   components:{
       'running-tasks':RunningTasks
+  },
+  methods:{
+    format(){
+        var jsonObject
+        try{
+            jsonObject = JSON.parse(this.taskJson);
+        }catch(e){
+            if(e.name ==="SyntaxError"){
+                this.jsonSyntaxError = true
+            }
+            return
+        }
+        if(typeof jsonObject === "object"){
+            this.taskJson = JSON.stringify(jsonObject,null,2);
+            this.jsonSyntaxError = false
+        }
+        //console.log('format:',this.dialogMessage)
+    },
+    showCreateTaskDialog(){
+        this.createTaskDialogVisible = true
+    },
+    creatTask(){
+        var postData = JSON.parse(this.taskJson)
+        this.$http.post(this.$common.apis.baseTaskUrl,postData,{
+                                                header:{ContentType:"application/json"}
+                                            }
+                        ).then(response =>{
+                            this.createTaskDialogVisible = false
+                            EventBus.$emit('updataAllTasks')
+                            //this.init()
+                            var successMessage = 'create successfully, taskId:' + response.data.task
+                            console.log("successMessage:",successMessage)
+                            this.$message({
+                                showClose: true,
+                                message: successMessage,
+                                type: 'success'
+                            })
+                            this.taskJson = ''
+                        },
+                        response =>{
+                            var errorMessage = 'request failed with status: ' + response.status + ' please check overlord logs'
+                            this.$message({
+                                showClose: true,
+                                message: errorMessage,
+                                duration:0,
+                                type: 'error'
+                            })
+                        }
+                        )
+    },
   }
 }
 </script>
--- src/components/data/access/taskmanager/RunningTasks.vue
+++ src/components/data/access/taskmanager/RunningTasks.vue
@@ -27,49 +27,6 @@
         <br>
     </div> 
 
-    <div style=" margin-left:20px;">
-        <span style="color: #242f42;font-size:20px;" ><b>Complete Tasks - Tasks recently completed</b></span>
-        <br></br>
-    </div>
-     <div class="table" style=" margin-left:20px;">
-        <el-table :data="completeTasks" border stripe style="width: 100%">
-            <el-table-column prop="id" label="id" min-width="400"></el-table-column>
-            <el-table-column prop="status" label="status" width="110"></el-table-column>
-            <el-table-column sortable prop="createdTime" label="createdTime" width="207"></el-table-column>
-            <el-table-column prop="duration" label="duration" width="110"></el-table-column>
-            
-            <el-table-column prop="topic" label="topic" width="207"></el-table-column>
-            <el-table-column prop="offsets" label="offsets" width="150"></el-table-column>
-
-            <el-table-column label="操作" width="275">
-                <template scope="scope">
-                    <el-button size="mini" @click="getTaskInfo(scope.row.id)">payload</el-button>
-                    <el-button size="mini" @click="getTaskStatus(scope.row.id)">status</el-button>
-                    <el-button size="mini" @click="getTasklog(scope.row.id,0)">log(all)</el-button>
-                    <el-button size="mini" @click="getTasklog(scope.row.id,8192)">log(8kb)</el-button>
-                </template>
-            </el-table-column>
-        </el-table>
-        <br>
-    </div> 
-
-
-    <div style=" margin-left:20px;">
-        <el-button type="primary" size="small" @click="showCreateTaskDialog">Create Task</el-button>
-    </div>
-
-    <el-dialog  :visible.sync="createTaskDialogVisible" size="small" title="Task Json" :close-on-click-modal="false">
-        <el-button type="primary" @click="format">format</el-button> 
-        <span style="color: red" v-if="jsonSyntaxError">json格式错误</span>
-        <br></br>             
-        <el-input type="textarea" :rows="25" v-model="taskJson">
-        </el-input>
-        <span slot="footer" class="dialog-footer">
-             <el-button @click="createTaskDialogVisible = false">取 消</el-button> 
-            <el-button type="primary" @click="creatTask()">创 建</el-button>
-        </span>
-    </el-dialog>
-
     <el-dialog  :visible.sync="dialogVisible" :size="dialogSize"  @close="dialogMessage = ''">
          <template slot="title">
             <div style=" line-height: 1;
@@ -89,7 +46,6 @@
         </span>
     </el-dialog>
     
-
   </div>
 </template>
 
@@ -100,21 +56,16 @@ export default {
   data () {
     return {
       runnigTasks:[],
-      completeTasks:[],
       taskInfo:{},
       taskStatus:{},
       taskLog:'',
       dialogMessage:'',
       dialogTitle:'',
       dialogSize:'full',
-      //autosize="{ minRows: 15, maxRows: 25}"
       dialogInputAutosize:{},
       dialogVisible:false,
-      canFormatted:false,
-      jsonSyntaxError: false,
-      loading:false,
-      createTaskDialogVisible:false,
-      taskJson:''
+      loading:false
+
     }
   },
 //   computed:{
@@ -128,8 +79,6 @@ export default {
   methods: { 
       init(){
         this.getRunningTasks()
-         console.log('running Tasks:',this.runnigTasks)
-        this.getCompleteTasks()
       },
       getRunningTasks(){
         this.$http.get(this.$common.apis.runnigTasks).then(response => {
@@ -181,37 +130,7 @@ export default {
         this.dialogSize = dialogSize
         this.dialogInputAutosize = dialogInputAutosize
       }, 
-      showCreateTaskDialog(){
-        this.createTaskDialogVisible = true
-      },
-      creatTask(){
-        var postData = JSON.parse(this.taskJson)
-        this.$http.post(this.$common.apis.baseTaskUrl,postData,{
-                                                header:{ContentType:"application/json"}
-                                            }
-                        ).then(response =>{
-                            this.createTaskDialogVisible = false
-                            this.init()
-                            var successMessage = 'create successfully, taskId:' + response.data.task
-                            console.log("successMessage:",successMessage)
-                            this.$message({
-                                showClose: true,
-                                message: successMessage,
-                                type: 'success'
-                            })
-                            this.taskJson = ''
-                        },
-                        response =>{
-                            var errorMessage = 'request failed with status: ' + response.status + ' please check overlord logs'
-                            this.$message({
-                                showClose: true,
-                                message: errorMessage,
-                                duration:0,
-                                type: 'error'
-                            })
-                        }
-                        )
-      },
+
       killTask(taskId){
         var remindMessage = "Do you really want to kill:" + "\n" + taskId.substring(0,45) 
                                 + "\n" + taskId.substring(45)
@@ -224,7 +143,8 @@ export default {
             var url = this.$common.apis.baseTaskUrl + "/" + taskId + "/shutdown"
             this.$http.post(url).then(
                 response => {
-                    this.init()
+                    window.setTimeout(this.init,200)
+                    //this.init()
                     this.$message({
                         type: 'success',
                         message: '删除成功!'
@@ -235,32 +155,13 @@ export default {
         }).catch(() => {
                 
         })
-      },
-      getCompleteTasks(){
-        this.$http.get(this.$common.apis.completeTasks).then(
-            response => {
-                this.completeTasks = []
-                this.$common.methods.pushData(response.data,this.completeTasks)
-               // console.log('completeTasks:',this.completeTasks)
-        })
-      },
-      format(){
-          var jsonObject
-          try{
-            jsonObject = JSON.parse(this.taskJson);
-          }catch(e){
-            if(e.name ==="SyntaxError"){
-                this.jsonSyntaxError = true
-            }
-            return
-          }
-          if(typeof jsonObject === "object"){
-            this.taskJson = JSON.stringify(jsonObject,null,2);
-            this.jsonSyntaxError = false
-          }
-          //console.log('format:',this.dialogMessage)
-      }
-   
+      }, 
+  },
+  mounted(){
+      var self = this
+      EventBus.$on("updataAllTasks",function(){
+          self.init()
+      });
   }
 }
 </script>
